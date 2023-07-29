63d1fbf15d5d20812a777cfadc0884e81d1351e3
Baekyohoon <qorygnsska@naver.com>
2023-07-29 16:25:03 +0900

    배교훈

--- src/main/java/com/example/demo/Project13Application.java
+++ src/main/java/com/example/demo/Project13Application.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Project13Application {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Project13Application.class, args);
+			SpringApplication.run(Project13Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/demo/controller/RecipeController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Recipe;
+import com.example.demo.repository.RecipeRepository;
+
+@Controller
+public class RecipeController {
+	 private final RecipeRepository recipeRepository;
+
+	    @Autowired
+	    public RecipeController(RecipeRepository recipeRepository) {
+	        this.recipeRepository = recipeRepository;
+	    }
+
+	    @GetMapping("/recipes")
+	    public String getAllRecipes(Model model) {
+	        List<Recipe> recipes = recipeRepository.findAll();
+	        model.addAttribute("recipes", recipes);
+	        return "recipeList"; // recipe.html을 렌더링하는 Thymeleaf 템플릿 이름
+	    }
+	}
+
