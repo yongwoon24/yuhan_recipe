153e44f8cce278fe65bcec1717e0e5ac22062bf9
jhhim <jhhim99@gmail.com>
2023-08-03 15:25:52 +0900

    카테고리 레시피 테이블

--- src/main/java/com/example/demo/entity/Recipe.java
+++ src/main/java/com/example/demo/entity/Recipe.java
@@ -11,43 +11,48 @@ public class Recipe {
     private String main_photo;
     private String created_date;
     private String user_id;
-
-    public int getRecipe_id() {
-        return recipe_id;
-    }
-    public void setRecipe_id(int recipe_id) {
-        this.recipe_id = recipe_id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getMain_photo() {
-        return main_photo;
-    }
-    public void setMain_photo(String main_photo) {
-        this.main_photo = main_photo;
-    }
-    public String getCreated_date() {
-        return created_date;
-    }
-    public void setCreated_date(String created_date) {
-        this.created_date = created_date;
-    }
-    public String getUser_id() {
-        return user_id;
-    }
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
-    @Override
-    public String toString() {
-        return "RecipeEntity [recipe_id=" + recipe_id + ", title=" + title + ", main_photo=" + main_photo
-                + ", created_date=" + created_date + ", user_id=" + user_id + "]";
-    }
-
-
+    private String category_name;
+	public int getRecipe_id() {
+		return recipe_id;
+	}
+	public void setRecipe_id(int recipe_id) {
+		this.recipe_id = recipe_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMain_photo() {
+		return main_photo;
+	}
+	public void setMain_photo(String main_photo) {
+		this.main_photo = main_photo;
+	}
+	public String getCreated_date() {
+		return created_date;
+	}
+	public void setCreated_date(String created_date) {
+		this.created_date = created_date;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	@Override
+	public String toString() {
+		return "Recipe [recipe_id=" + recipe_id + ", title=" + title + ", main_photo=" + main_photo + ", created_date="
+				+ created_date + ", user_id=" + user_id + ", category_name=" + category_name + "]";
+	}
+    
+   
 }
