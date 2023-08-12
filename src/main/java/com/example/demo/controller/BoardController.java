1332f9bc73a297fd168ff69172d25d90c6fe44e8
juice1342 <hongjoo0610@naver.com>
2023-08-12 19:25:53 +0900

    게시판목록

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Board;
+
+@Controller
+public class BoardController {
+	@GetMapping("/PostList")
+	public String PostBoard(Model model) {
+		model.addAttribute("board",new Board());
+		return "PostList";
+	}
 }
