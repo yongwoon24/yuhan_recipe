c8856b1f39334de59570b683269d9220bbde1280
jhhim <jhhim99@gmail.com>
2023-08-26 22:09:08 +0900

    ....수정

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,71 +2,85 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
-	@Id
-	private String user_id;
-	private String name;
-	private String email;
-	private String password;
-	private LocalDate birthdate;
-	private String phone_number;
-	private String verificationCode;
-	
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public LocalDate getBirthdate() {
-		return birthdate;
-	}
-	public void setBirthdate(LocalDate birthdate) {
-		this.birthdate = birthdate;
-	}
-	public String getPhone_number() {
-		return phone_number;
-	}
-	public void setPhone_number(String phone_number) {
-		this.phone_number = phone_number;
-	}
-	
-	
+   @Id
+   private String user_id;
+   private String name;
+   private String email;
+   private String password;
+   private LocalDate birthdate;
+   private String phone_number;
+   
+   
+   @Column(length = 36, name = "verification_token")
+    private String verificationToken;
 
-	public String getVerificationCode() {
-		return verificationCode;
-	}
-	public void setVerificationCode(String verificationCode) {
-		this.verificationCode = verificationCode;
-	}
-	@Override
-	public String toString() {
-		return "User [user_id=" + user_id + ", name=" + name + ", email=" + email + ", password=" + password
-				+ ", birthdate=" + birthdate + ", phone_number=" + phone_number + "]";
-	}
-	
-	
-	
+    private boolean emailVerified;
+
+
+    public String getVerificationToken() {
+        return verificationToken;
+    }
+
+    public void setVerificationToken(String verificationToken) {
+        this.verificationToken = verificationToken;
+    }
+
+  
+    
+
+   public String getUser_id() {
+      return user_id;
+   }
+   public void setUser_id(String user_id) {
+      this.user_id = user_id;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getEmail() {
+      return email;
+   }
+   public void setEmail(String email) {
+      this.email = email;
+   }
+   public String getPassword() {
+      return password;
+   }
+   public void setPassword(String password) {
+      this.password = password;
+   }
+   public LocalDate getBirthdate() {
+      return birthdate;
+   }
+   public void setBirthdate(LocalDate birthdate) {
+      this.birthdate = birthdate;
+   }
+   public String getPhone_number() {
+      return phone_number;
+   }
+   public void setPhone_number(String phone_number) {
+      this.phone_number = phone_number;
+   }
+   public boolean isEmailVerified() {
+      return emailVerified;
+   }
+   public void setEmailVerified(boolean emailVerified) {
+      this.emailVerified = emailVerified;
+   }
+   @Override
+   public String toString() {
+      return "User [user_id=" + user_id + ", name=" + name + ", email=" + email + ", password=" + password
+            + ", birthdate=" + birthdate + ", phone_number=" + phone_number + ", emailVerified=" + emailVerified
+            + "]";
+   }
+
 }
