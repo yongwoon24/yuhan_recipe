e6a4225e190dcc488406c411bce698592887a184
jhhim <jhhim99@gmail.com>
2023-09-12 20:48:19 +0900

    게시판 코드 추가

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -46,6 +48,27 @@ public class BoardController {
 		 }
     }
 	
+	
+	@GetMapping("/boardcontent/{postId}")
+    public String viewBoard(@PathVariable Long postId, Model model) {
+        // postId를 사용하여 보드 내용을 데이터베이스에서 가져온다.
+        Optional<Board> optionalBoard = boardRepository.findById(postId);
+        
+        if (optionalBoard.isPresent()) {
+            Board board = optionalBoard.get();
+
+            System.out.println("Board Title: " + board.getTitle());
+            System.out.println("Board Content: " + board.getContent());
+            // 모델에 보드 내용을 추가하여 템플릿에서 사용할 수 있도록 한다.
+            model.addAttribute("board", board);
+            return "boardcontent"; // viewboard.html 템플릿으로 이동
+        } else {
+            // 보드가 존재하지 않을 경우 에러 처리
+            return "board"; // 에러 페이지로 이동하거나 다른 처리를 수행
+        }
+    }
+	
+	
 	@PostMapping("/createboard")
 	public String createBoard(@ModelAttribute Board board, HttpSession session) {
 		
@@ -65,3 +88,5 @@ public class BoardController {
 	    return "redirect:/board"; // 글쓰기 성공 후 게시판 목록 페이지로 리다이렉트
 	}
 }
+
+
