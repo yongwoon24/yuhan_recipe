d5426c86f08b850f1b787413c1176a62402f1e81
tjdwn518 <tjdwn518@naver.com>
2023-11-06 20:50:34 +0900

    d

--- src/main/java/com/example/demo/controller/LoveController.java
+++ src/main/java/com/example/demo/controller/LoveController.java
@@ -53,6 +53,8 @@ public class LoveController {
 		  List<Recipe> todayrecipes =new ArrayList<>(); 
 		  Today today = todayR.findByUser(user);
 		  
+		  
+		  
 		  if (today != null) {
 		  todayrecipes.add(recipeRepository.findById(today.getNo1()));
 		  todayrecipes.add(recipeRepository.findById(today.getNo2()));
@@ -67,6 +69,21 @@ public class LoveController {
 		  
 		  model.addAttribute("todayrecipes", todayrecipes);
 		  }
+		  if(loggedInUserId==null){
+			  user = userR.findByUser_id1("admin");
+			  today = todayR.findByUser(user);
+			  todayrecipes.add(recipeRepository.findById(today.getNo1()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo2()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo3()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo4()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo5()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo6()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo7()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo8()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo9()));
+		  todayrecipes.add(recipeRepository.findById(today.getNo10()));
+		  
+		  model.addAttribute("todayrecipes", todayrecipes);}
 		  
     	//최근본 레시피
     	List<Recipe> lastrecipes = loveRepository.findUserActivitiesWithdesc(user);
